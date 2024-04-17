a64c87635101bb54efc8f947a7a6da39c2c8fc3c
Vinigperuzzi <vinigperuzzi@hotmail.com>
2024-04-17 01:41:30 -0300

    ADD: user sign in and sign up ADD: better style for views and forms

---
+++ spec/system/user_sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+  it 'com sucesso' do
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Criar uma conta'
+    fill_in 'E-mail', with: 'vini@email.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar conta'
+
+    expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
+    expect(page).to have_content 'vini@email.com'
+    expect(page).to have_button 'Sair'
+    user = User.last
+    expect(user.email).to eq "vini@email.com"
+  end
+end
