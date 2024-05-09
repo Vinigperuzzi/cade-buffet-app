35cff6b02065dd95a5d6850c46f1a79ce696c055
Vinigperuzzi <vinigperuzzi@hotmail.com>
2024-05-09 02:32:10 -0300

    ADD: better navigation on chat screen (back to order button)

--- spec/system/chat/user_access_chat_spec.rb
+++ spec/system/chat/user_access_chat_spec.rb
@@ -353,4 +353,35 @@ describe 'User access chat' do
     expect(current_path).to eq root_path
     expect(page).to have_content 'Você não permissão para editar essa mensagem.'
   end
+
+  it "and can get back to order's page" do
+    customer = Customer.create!(email: 'cliente@email.com', password: 'password', name: 'cliente', cpf: '479.111.310-15')
+    user = User.create!(email: 'vinicius@email.com', password: 'password')
+    buffet = Buffet.create!(name: 'Vini', corporate_name: 'Vinícius Gourmet alimentos', 
+                          register_number: '12456456000145', phone: '53 991814646', email: 'vinigperuzzi@gourmet.com',
+                          address: 'Estrada do Laranjal, 695', district: 'Laranjal', state: 'RS', city: 'Pelotas',
+                          payment_method: 'Pix, Débito, Crédito, Dinheiro', description: 'O melhor serviço de buffet do centro de Pelotas')
+    user.update!(buffet_id: buffet.id)
+    event = Event.create!(name: 'Casamento', description: 'Serviço de mesa completo para casamentos', min_qtd: 20, max_qtd: 40,
+                            duration: 250, menu: 'Frutos do Mar', buffet_id: buffet.id)
+    price = Price.create!(base_price: 5000, sp_base_price:6000, sp_additional_person:500,
+                          additional_person:200, sp_extra_hour:30, extra_hour:20, event_id: event.id)
+    album = Album.create!(event_id: event.id)
+    album.images.attach(io: File.open(Rails.root.join('spec', 'support', 'festa_casamento.jpg')), filename: 'festa_casamento.jpg')
+
+    order = Order.create!(buffet_id: buffet.id, event_id: event.id, event_date: Date.new(2134, 5, 1), estimated_qtd: 30,
+                          event_details: 'Esse evento deve possuir toda a temática com corações rosa', address: '', order_status: :waiting,
+                          final_price: 11000, extra_tax: 1000, discount: 500, customer_id: customer.id, out_doors: false)
+
+    login_as user, scope: :user
+    visit root_path
+    click_on 'Pedidos'
+    click_on 'Mostrar Detalhes'
+    click_on 'Conversar com o cliente sobre esse pedido'
+    fill_in "message_text", with: 'Tudo bem, e com você?'
+    click_on 'Enviar mensagem'
+    click_on 'Voltar ao pedido'
+
+    expect(current_path).to eq order_path(order.id)
+  end
 end
